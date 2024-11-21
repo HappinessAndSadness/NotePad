a557211064635adfc894101ef8ca8379e91b6e24
HappinessAndSadness <407916586@qq.com>
2024-11-21 23:21:52 +0800

    Update NoteColor.java

--- app/src/main/java/com/example/android/notepad/NoteColor.java
+++ app/src/main/java/com/example/android/notepad/NoteColor.java
@@ -12,59 +12,63 @@ public class NoteColor extends Activity {
     private Uri mUri;
     private int color;
     private static final int COLUMN_INDEX_TITLE = 1;
-    private static final String[] PROJECTION = new String[] {
+    private static final String[] PROJECTION = new String[]{
             NotePad.Notes._ID, // 0
             NotePad.Notes.COLUMN_NAME_BACK_COLOR,
     };
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.note_color);
-        //Uri passed in from noteeditor
         mUri = getIntent().getData();
-        mCursor = managedQuery(
-                mUri,        // The URI for the note that is to be retrieved.
-                PROJECTION,  // The columns to retrieve
-                null,        // No selection criteria are used, so no where columns are needed.
-                null,        // No where columns are used, so no where values are needed.
-                null         // No sort order is needed.
-        );
+        mCursor = getContentResolver().query(mUri, PROJECTION, null, null, null);
     }
+
     @Override
-    protected void onResume(){
-        //The execution order is after oncreate
+    protected void onResume() {
         if (mCursor != null) {
             mCursor.moveToFirst();
-            color = mCursor.getInt(COLUMN_INDEX_TITLE);
+            //color = mCursor.getInt(COLUMN_INDEX_TITLE);
+            color = mCursor.getInt(1); // 1是COLUMN_NAME_BACK_COLOR的索引
         }
         super.onResume();
     }
+
     @Override
     protected void onPause() {
-        //After finish(), the selected color is stored in the database
         super.onPause();
+
+        if (mCursor != null) {//
+            mCursor.close();//
+        }//
+
         ContentValues values = new ContentValues();
         values.put(NotePad.Notes.COLUMN_NAME_BACK_COLOR, color);
         getContentResolver().update(mUri, values, null, null);
     }
-    public void white(View view){
+
+    public void white(View view) {
         color = NotePad.Notes.DEFAULT_COLOR;
         finish();
     }
-    public void yellow(View view){
+
+    public void yellow(View view) {
         color = NotePad.Notes.YELLOW_COLOR;
         finish();
     }
-    public void blue(View view){
+
+    public void blue(View view) {
         color = NotePad.Notes.BLUE_COLOR;
         finish();
     }
-    public void green(View view){
+
+    public void green(View view) {
         color = NotePad.Notes.GREEN_COLOR;
         finish();
     }
-    public void red(View view){
+
+    public void red(View view) {
         color = NotePad.Notes.RED_COLOR;
         finish();
     }
-
 }
